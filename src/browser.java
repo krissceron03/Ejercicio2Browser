d35ae0a28ff9ce0830c2246dfbe6da17111b5757
Kriss Cerón <krissceron03@outlook.com>
2023-04-08 19:36:02 -0500

    Subiendo ejercicio 2 Browser con funciones

--- src/browser.java
+++ src/browser.java
@@ -4,7 +4,6 @@ import java.util.Stack;
 
 public class browser extends JDialog {
     private JPanel contentPane;
-    private JButton buttonOK;
     private JButton buttonCancel;
     private JTextField txtUrl;
     private JButton btnEnter;
@@ -14,18 +13,10 @@ public class browser extends JDialog {
     Stack <String> visitados=new Stack<String>();
     Stack <String> back=new Stack<String>();
 
-
-
-
-
-
-
     public browser() {
         setContentPane(contentPane);
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
-
-
+        //getRootPane().setDefaultButton(buttonOK);
 
         buttonCancel.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -49,105 +40,100 @@ public class browser extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
 
-btnAtras.setEnabled(false);
-btnDelante.setEnabled(false);
+        btnAtras.setEnabled(false);
+        btnDelante.setEnabled(false);
+        //CUANDO PRESIONAMOS ENTER
         btnEnter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //descripcion.setText(txtUrl.getText());
                 //String url=txtUrl.getText();
                 btnAtras.setEnabled(true);
-
-
-                String url=txtUrl.getText();
-                if(url.substring(0,4).equals("www.")&& url.substring(url.length()-4,url.length()).equals(".com")){
-                    visitados.push(txtUrl.getText());
-
-                    System.out.println(visitados);
-
-
-                    String cadena="";
-                    for (String vis:visitados) {
-                        cadena= cadena+vis.substring(4,vis.length()-4)+", ";
-
-                    }
-                    descripcion.setText(cadena);
-                }else{
-
-                        JOptionPane.showMessageDialog(null,"ERROR, INGRESE LA URL CORRECTAMENTE");
-
-                    if (visitados.size()>0){
-                        txtUrl.setText(visitados.peek());
-                    }else{
-                        txtUrl.setText(null);
-                    }
-
-
-                }
-
+                onEnter();
             }
         });
+
+        //CUANDO PRESIONAMOS ATRÁS
         btnAtras.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 btnDelante.setEnabled(true);
-                String cadena;
-                if(visitados.size()>1) {
-                    back.push(visitados.peek());
-                    System.out.println(back);
-                    visitados.pop();
-                    cadena = " ";
-                    for (String vis : visitados) {
-                        cadena = cadena + vis.substring(4, vis.length() - 4) + ", ";
-
-                    }
-                    descripcion.setText(cadena);
-                    txtUrl.setText(visitados.peek());
-                }else {
-                    back.push(visitados.peek());
-                    visitados.pop();
-                    txtUrl.setText(null);
-                    descripcion.setText(null);
-                    btnAtras.setEnabled(false);
-
-                }
-
+                onAtras();
             }
         });
+
+        //CUANDO PRESIONAMOS SIGUIENTE
         btnDelante.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
              btnAtras.setEnabled(true);
-                String cadena;
-                if (back.size()>0) {
-                    visitados.push(back.peek());
-                    back.pop();
-                    if(back.size()==0){
-                        btnDelante.setEnabled(false);
-                    }
-                    cadena = " ";
-                    for (String vis : visitados) {
-                        cadena = cadena + vis.substring(4, vis.length() - 4) + ", ";
-
-                    }
-                    descripcion.setText(cadena);
-                    txtUrl.setText(visitados.peek());
-                }
-
-
+             onSiguiente();
             }
         });
     }
+    //FUNCIONES:
+    //FUNCIÓN PARA ENTER
+    private void onEnter(){
+        String url=txtUrl.getText();
+        if(url.substring(0,4).equals("www.")&& url.substring(url.length()-4,url.length()).equals(".com")){
+            visitados.push(txtUrl.getText());
+            System.out.println(visitados);
+            recorrerPila();
+        }else{
+            JOptionPane.showMessageDialog(null,"ERROR, INGRESE LA URL CORRECTAMENTE");
+            if (visitados.size()>0){
+                txtUrl.setText(visitados.peek());
+            }else{
+                txtUrl.setText(null);
+            }
+        }
+    }
+    //FUNCIÓN PARA ATRÁS
+    private void onAtras(){
+        if(visitados.size()>1) {
+            back.push(visitados.peek());
+            System.out.println(back);
+            visitados.pop();
+            recorrerPila();
+            txtUrl.setText(visitados.peek());
+        }else {
+            back.push(visitados.peek());
+            visitados.pop();
+            txtUrl.setText(null);
+            descripcion.setText(null);
+            btnAtras.setEnabled(false);
 
-    private void onOK() {
-        // add your code here
-        dispose();
+        }
     }
 
+    //FUNCIÓN PARA SIGUIENTE
+    private void onSiguiente(){
+        if (back.size()>0) {
+            visitados.push(back.peek());
+            back.pop();
+            if(back.size()==0){
+                btnDelante.setEnabled(false);
+            }
+            recorrerPila();
+            txtUrl.setText(visitados.peek());
+        }
+    }
+
+    //FUNCIÓN PARA HACER UN BARRIDO DE TODA LA PILA, ALMACENAR CADA STRING EN LA VARIABLE VIS Y ALMACENAR LA PALABRA QUE HAY DESDE LA 4TA POSICIÓN HASTA LA LONG DE LA PALABRA -4
+    private void recorrerPila(){
+        String cadena=" ";
+        for (String vis : visitados) {
+            cadena = cadena + vis.substring(4, vis.length() - 4) + ", ";
+        }
+        descripcion.setText(cadena);
+    }
+
+    //CUANDO PRESIONAMOS SALIR
     private void onCancel() {
         // add your code here if necessary
         dispose();
     }
+
 
     public static void main(String[] args) {
         browser dialog = new browser();
